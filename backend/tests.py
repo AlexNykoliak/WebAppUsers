193efdf43895ea32f4c0d7fd7e81e97ddfbac308
OLeksandr Nykolyak <nykolyak.o@gmail.com>
2022-10-13 15:49:06 +0300

    add urls

--- backend/tests.py
+++ backend/tests.py
@@ -1,2 +1,11 @@
-from django.test import TestCase
 
+
+
+"""
+
+import xml.etree.ElementTree as ET
+tree = ET.parse('https://drive.google.com/file/d/1BhxlRfWe7EsnkIh5BiGwZfUp6v0P6BnI/view?usp=sharing')
+root = tree.getroot()
+print(root)
+
+"""
