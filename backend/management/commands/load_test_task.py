09308828f86a3dd114946af1165bb83069a10a1c
OLeksandr Nykolyak <nykolyak.o@gmail.com>
2022-10-13 13:19:49 +0300

    Import CSV into Django Database

---
+++ backend/management/commands/load_test_task.py
@@ -0,0 +1,32 @@
+from csv import DictReader
+from django.core.management import BaseCommand
+
+# Import the model
+from backend.models import User
+
+ALREDY_LOADED_ERROR_MESSAGE = """
+If you need to reload the task test data from the CSV file,
+first delete the db.sqlite3 file to destroy the database.
+Then, run `python manage.py migrate` for a new empty
+database with tables"""
+
+
+class Command(BaseCommand):
+    # Show this when the user types help
+    help = "Loads data from test_task.csv"
+
+    def handle(self, *args, **options):
+
+        # Show this if the data already exist in the database
+        if User.objects.exists():
+            print('User data already loaded...exiting.')
+            print(ALREDY_LOADED_ERROR_MESSAGE)
+            return
+
+        # Show this before loading the data into the database
+        print("Loading User data")
+
+        # Code to load the data into database
+        for row in DictReader(open('./test_task.csv')):
+            user = User(username=row['username'], password=row['password'], date_joined=row['date_joined'])
+            user.save()
