af45da14a328364817adffeb71207c45ce88e79f
OLeksandr Nykolyak <nykolyak.o@gmail.com>
2022-10-13 16:16:42 +0300

    add views&templates[frontend]

--- frontend/views.py
+++ frontend/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from backend.models import User
+
+
+class UserListView(ListView):
+    model = User
+    template_name = 'home.html'
+
+
+
+
